cf5f9b2cc825a959b0a4333e73946c04bfea5347
Octahedron-apple <bhavya1singh1@gmail.com>
2026-04-04 00:29:45 +0530

    defined the static dir and template dir in code

--- app.py
+++ app.py
@@ -1,8 +1,33 @@
-import numpy
-import pandas
-from fastapi import FastAPI
+import os
+import joblib
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
+
 app = FastAPI()
-@app.get("/")
-async def home():
-    pass
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="template")
+
+class SurveyData(BaseModel):
+    region: str
+    district: str
+    geography: str
+    gender: str
+    age_band: str
+    age: float
+    caste: str
+    occupation: str
+    cm_preference: str
+    voting_reason: str
+
+# model = joblib.load("models/model.pkl")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
